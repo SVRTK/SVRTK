122f07896e6c1fdffacfc63a85b42bb79ff45afb
Alena Ulla Uus <41996502+alenauus@users.noreply.github.com>
2019-01-18 17:32:46 +0000

    v1.0

--- include/mirtk/ReconstructionCardiac4D.h
+++ include/mirtk/ReconstructionCardiac4D.h
@@ -15,7 +15,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */
+ */ 
 
 #ifndef MIRTK_ReconstructionCardiac4D_H
 #define MIRTK_ReconstructionCardiac4D_H
